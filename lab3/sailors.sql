3d5bba850a9ba5d5898023fc499b0f09343ff9d7
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2023-09-26 09:54:34 +0530

    Update sailors.sql

--- lab3/sailors.sql
+++ lab3/sailors.sql
@@ -1,3 +1,36 @@
+-- Data Definition Language (DDL) commands in RDBMS
+
+--Consider the database schemas given below.
+
+-- Write ER diagram and schema diagram. The primary keys are 
+
+--underlined and the data types are specified.
+
+--Create tables for the following schema listed below by properly 
+
+--specifying the primary keys and foreign keys.
+
+--Enter at least five tuples for each relation.
+
+--Altering tables,
+
+--Adding and Dropping different types of constraints.
+
+--Also adding and dropping fields in to the relational schemas of the listed
+--problems.
+
+--Delete, Update operations
+
+--  A.Sailors database
+
+--SAILORS (sid, sname, rating, age)
+
+--BOAT(bid, bname, color)
+
+--RSERVERS (sid, bid, date)
+
+
+
 CREATE DATABASE Sailors;
 USE SAILORS;
 
