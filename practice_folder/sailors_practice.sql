1c5a552e3c31ca517f636b3438640da42e0bbbb2
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2023-12-15 10:14:47 +0530

    Update sailors_practice.sql

--- practice_folder/sailors_practice.sql
+++ practice_folder/sailors_practice.sql
@@ -1,4 +1,5 @@
-/*1. Consider the database schemas given below.
+/*
+1. Consider the database schemas given below.
 Write ER diagram and schema diagram. The primary keys are underlined and the data types are specified.
 Create tables for the following schema listed below by properly specifying the primary keys and foreign keys.
 Enter at least five tuples for each relation.
@@ -27,6 +28,7 @@ SELECT DISTINCT BOAT.color
 +-------+
 | Green |
 +-------+
+  
 /* 2.	Find all sailor id’s of sailors who have a rating of at least 4 or reserved boat 701*/
 SELECT DISTINCT SAILORS.sid 
   FROM SAILORS LEFT JOIN RSERVERS ON SAILORS.sid= RSERVERS.sid 
@@ -38,7 +40,8 @@ SELECT DISTINCT SAILORS.sid
 | 602 |
 | 604 |
 +-----+
-/* Find the names of sailors who have not reserved a boat whose name contains the string “storm”. Order the names in ascending order. */
+  
+/* 3.Find the names of sailors who have not reserved a boat whose name contains the string “storm”. Order the names in ascending order. */
 SELECT S.sname FROM SAILORS S WHERE S.sid NOT IN 
   (SELECT R.sid FROM RSERVERS R JOIN BOAT B ON R.bid = B.bid 
   WHERE B.bname LIKE '%at2%' ) 
@@ -66,4 +69,46 @@ WHERE NOT EXISTS (
 );
 Empty set (0.00 sec)
 
+-- 5.	Find the name and age of the oldest sailor. 
+SELECT sname, age FROM SAILORS ORDER BY age DESC LIMIT 1;
++-------+------+
+| sname | age  |
++-------+------+
+| JAMES |   31 |
++-------+------+
 
+-- 6.	For each boat which was reserved by at least 5 sailors with age >= 40, find the boat id and the average age of such sailors.
+ SELECT R.bid AS boat_id, AVG(S.age) 
+  AS average_age FROM RSERVERS R 
+  JOIN SAILORS S ON R.sid = S.sid 
+  WHERE S.age >= 40 
+  GROUP BY R.bid HAVING COUNT(DISTINCT R.sid) >= 5;
+Empty set (0.00 sec)
+
+-- 7.	Create a view that shows the names and colours of all the boats that have been reserved by a sailor with a specific rating.
+-- Create a view named 'ReservedBoatsByRating'
+mysql> CREATE VIEW ReservedBoatsByRating AS
+     SELECT DISTINCT B.bname AS boat_name, B.color
+     FROM RSERVERS R
+     JOIN SAILORS S ON R.sid = S.sid
+    JOIN BOAT B ON R.bid = B.bid
+   WHERE S.rating = 4.5;
+
+-- 8.	A trigger that prevents boats from being deleted If they have active reservations. 
+DELIMITER //
+mysql> CREATE TRIGGER prevent_delete_active_reservations
+    -> BEFORE DELETE ON BOAT
+    -> FOR EACH ROW
+    -> BEGIN
+    ->     -- Check if the boat has active reservations
+    ->     IF (SELECT COUNT(*) FROM RSERVERS WHERE bid = OLD.bid) > 0 THEN
+    ->         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Cannot delete boat with active reservations';
+    ->     END IF;
+    -> END;
+    -> //
+      
+Query OK, 0 rows affected (0.08 sec)
+
+mysql> DELIMITER ;
+mysql> DELETE FROM BOAT WHERE bid=705;
+ERROR 1644 (45000): Cannot delete boat with active reservations
